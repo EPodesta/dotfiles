f8d89ba3a82697b1cfa22a73e6729d28c1cd226e
Emmanuel Podestá Junior <epodesta158@gmail.com>
2022-04-19 15:24:30 -0300

    Added i3 config files 2022.

--- .config/i3/i3lock.sh
+++ .config/i3/i3lock.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
- 
-scrot /tmp/screen.png
-convert /tmp/screen.png -scale %10 -scale 1000% /tmp/screen.png
-[[ -f $1 ]] && convert /tmp/screen.png $1 -gravity center -composite -matte /tmp/screen.png
-i3lock -u -i /tmp/screen.png
-rm /tmp/screen.png
+
+sleep 0.2
+# Suspend dunst and lock, then resume dunst when unlocked.
+pkill -u $USER -USR1 dunst
+i3lock -n -k -F -i $HOME/Pictures/j0uBATj.png \
+    --inside-color=373445ff --ring-color=ffffffff --line-uses-inside \
+    --keyhl-color=d23c3dff --bshl-color=d23c3dff --separator-color=00000000 \
+    --insidever-color=373445ff --verif-color=373445ff --insidewrong-color=d23c3dff \
+    --ringver-color=ffffffff --ringwrong-color=ffffffff --ind-pos="x+40:y+735" \
+    --radius=15 --verif-text="" --wrong-text="" --noinput-text="" \
+    --time-pos="x+700:y+270" --time-font=Fira Code Nerd Font \
+    --date-font=Fira Code Nerd Font --time-size=48 --time-str="%H:%M"
+pkill -u $USER -USR2 dunst
+
+# Revert compton's config changes.
+sleep 0.2
